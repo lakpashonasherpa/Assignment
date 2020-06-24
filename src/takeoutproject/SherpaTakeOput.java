6347d6320c445b2665425c52cfdcccf7c366c7ab
Lakpa <lakpashonasherpa@gmail.com>
2020-06-24 15:35:16 -0400

    second commit on takeout package

--- src/takeoutproject/SherpaTakeOput.java
+++ src/takeoutproject/SherpaTakeOput.java
@@ -1,5 +1,69 @@
 package takeoutproject;
 
 public class SherpaTakeOput {
+public  void displayMainMenu() {
+	System.out.println("The following are the Food Menu that we serve");
+	System.out.println("1. Italian Food ");
+	System.out.println("2. American Food ");
+	System.out.println("3. Chinese Food ");
+}
+public  void displayItalianFoodMenu() {
+	System.out.println("Italian Food Lists");
+	System.out.println("------------------");
+	System.out.println("1.Lasagna Tray ");
+	System.out.println("2.Pizza Pack");
+	System.out.println("3.Gazpacho Soup, salad and bread sticks pack");
+	//System.out.println("Choose what type of food you'd like to order:");
+
+}
+public  void displayChineseMenu() {
+	System.out.println("Chinese Food Lists");
+	System.out.println("-------------------");
+System.out.println("1.Chicken and Broccoli Tray(includes 7 wonton soups, 7 egg rolls)");
+System.out.println("2.Sweet and Sour Pork Tray(includes 7 hot and sour soupd, 7 egg rools)");
+System.out.println("3.Shrimp Fried Rice Tray(includes 10 egg rolls)");
+
+}
+public  void displayAmericanMenu() {
+	System.out.println("American Food Lists");
+	System.out.println("--------------------");
+	System.out.println("1.Hamburger and Hot Dog Tray-includes buns and condiments");
+	System.out.println("2.Grilled Chicken Sandwich and Mozzarella Sticks Tray-includes dipping sauces");
+	System.out.println("3.Barbeque Tray-includes buns and peach cobber");
+}
+public int determineTrays(int people, int feeds) {
+	int trays=people/feeds;
+
+	return trays;
+}
+public  double getSubtotal(double price, int trays) {
+	
+	double costs=price*trays;
+	
+	return costs;
+
+}
+
+public  double getTax(double subtotal, double taxrate) {
+	double tax= subtotal*taxrate;
+	return tax;
+}
+public  double getTips(double subtotal, double tiprate) {
+	double tips=subtotal*tiprate;
+	
+	return tips;
+}
+public  double getGrandTotal(double subtotal, double tax, double tip) {
+	double total=subtotal+tax+tip;
+	return total;
+}
+public  double pricePerPerson(double grandTotal, int people) {
+	double pricePerson =grandTotal/people;
+	return pricePerson;
+}
+public  int determineLeftOvers(int feeds, int trays, int people) {
+	
+	return 0;
+}
 
 }
--- src/takeoutproject/TestTakeOut.java
+++ src/takeoutproject/TestTakeOut.java
@@ -1,5 +1,245 @@
 package takeoutproject;
 
+import java.util.Scanner;
+
 public class TestTakeOut {
+	public static void main(String[] args) {
+		SherpaTakeOput  obj=new SherpaTakeOput();
+		Scanner input=new Scanner(System.in);
+		System.out.println("Enter the number of people would you like to order for");
+		int people=input.nextInt();
+		
+		if(people>=10 && people<=100) {
+
+		obj.displayMainMenu();
+		System.out.println("CHOSE TYPE OF MEAL");
+		int choice=input.nextInt();
+		int feeds,trays;
+		double price, subtotal,tax,tips,total,perperson;
+		double taxrate=0.07;
+		double tiprate=0.15;
+		
+		switch (choice) {
+		case 1:
+			
+			obj.displayItalianFoodMenu();
+			System.out.println("CHOSE ONE");
+			choice=input.nextInt();
+			if(choice==1) {
+				feeds=5;
+				price=17.99;
+				
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+				
+			}
+			if(choice==2) {
+				feeds=7;
+				price=15.99;	
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+			}
+			if(choice==3) {
+				feeds=4;
+				price=12.99;
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+				
+			}
+			
+			break;
+		case 2:
+			obj.displayAmericanMenu();
+			System.out.println("CHOSE ONE");
+			choice=input.nextInt();
+			if(choice==1) {
+				feeds=7;
+				price=18.99;
+				
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+				
+			}
+			if(choice==2) {
+				feeds=5;
+				price=22.99;	
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+			}
+			if(choice==3) {
+				feeds=10;
+				price=26.99;
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+				
+			}
+
+			
+			break;
+		case 3:			
+			obj.displayChineseMenu();
+			System.out.println("CHOSE ONE");
+			choice=input.nextInt();
+			if(choice==1) {
+				feeds=7;
+				price=18.99;
+				
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+				
+			}
+			else if(choice==2) {
+				feeds=7;
+				price=18.99;	
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+			}
+			else if(choice==3) {
+				feeds=5;
+				price=18.99;
+				trays=obj.determineTrays(people, feeds);
+				subtotal=obj.getSubtotal(price, trays);
+				tax=obj.getTax(subtotal, taxrate);
+				tips=obj.getTips(subtotal, tiprate);
+				total=obj.getGrandTotal(subtotal, taxrate, tips);
+				perperson=obj.pricePerPerson(total, people);
+				
+				
+				System.out.println("You need "+trays+" trays");
+				System.out.println("Feeds: "+feeds);
+				System.out.println("Price for "+people+" people ("+trays+") trays is: $"+subtotal);
+				System.out.println("Tax :$"+tax);
+				System.out.println("Tip : $"+tips);
+				System.out.println("Total (food, tax,tip): $"+total);
+				System.out.println("Price per person: $"+perperson);
+				
+			}
+			else {
+				System.out.println("enter the valide choice");
+			}
+						
+			break;
+		default:
+			System.out.println("Please select the right menu");
+			break;
+			}
+		
+		}	else
+		{
+			System.out.println("you have enter the invalid number.");
+		}
+
+		
+		
+		
+		
+		System.out.println("Thanks you for your business with us!!!!");
+		input.close();
+		
+	}
 
 }
