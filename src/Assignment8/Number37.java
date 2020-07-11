e2935c215d5c94259882a92246a6cfdd4e1fdf4f
Lakpa <lakpashonasherpa@gmail.com>
2020-07-10 21:38:19 -0400

    assignment updates

---
+++ src/Assignment8/Number37.java
@@ -0,0 +1,24 @@
+package Assignment8;
+
+public class Number37 {
+	final static int ROW = 3;
+	final static int COLUMN = 3;
+
+	public static void sumOfMultiDArray(int[][] array1, int[][] array2) {
+		int[][] sum = new int[ROW][COLUMN];
+		for (int i = 0; i < ROW; i++) {
+			for (int j = 0; j < COLUMN; j++) {
+				sum[i][j] = array1[i][j] + array2[i][j];
+				System.out.print(sum[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		int[][] arr1 = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		int[][] arr2 = new int[][] { { 4, 5, 6 }, { 2, 7, 8 }, { 3, 1, 9 } };
+		sumOfMultiDArray(arr1, arr2);
+
+	}
+}
---
+++ src/assignment9/Test.java
@@ -0,0 +1,30 @@
+package assignment9;
+
+
+
+public class Test {
+	public static void main(String[] args) {
+//		BankAccount ob=new BankAccount(0123,"lakpa");
+//		ob.deposit(50000.00);
+//		ob.withdraw(20000.00);
+//		ob.display();
+//		
+//		System.out.println("=========================");
+//		ob.deposit(25000, 3);
+//		ob.display();
+//		
+	System.out.println("==========================");
+	BankAccount ob1=new BankAccount(221122,"John",10000.00);
+     ob1.deposit(10000, 3);
+     ob1.withdraw(1000, 0.5);
+     ob1.display();
+     
+//     
+//     System.out.println("=======================");
+//	BankAccount obj2=new BankAccount(01203,"Ram",45000);
+//	obj2.display();
+//		
+
+	}
+
+}
